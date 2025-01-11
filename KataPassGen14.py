c0daa72a03a21039bb6d8a6f3b14cff70cc4bd56
Anton Solovev <48914909+Lindengrounder@users.noreply.github.com>
2025-01-11 15:26:42 +0300

    Update KataPassGen14.py
    Применение констант для обозначения символов способствует большей читаемости кода. Разделение логики на функции улучшает структуру и повышает возможность повторного использования кода. Теперь можно легко менять длину и набор символов пароля. Перемешивание символов в конце обеспечивает большую случайность пароля.

--- KataPassGen14.py
+++ KataPassGen14.py
@@ -1,23 +1,39 @@
 # KataPassGen14.py
 # 11/01/2025 - протестирована на онлайн компиляторе JDoodle.com
+# 11/05/2025 - улучшенная версия
 
 import random
 from random import choice
-from string import ascii_uppercase
+from string import ascii_uppercase, ascii_lowercase, digits
 
-def test():
-    cs = random.randint(2,9)
-    symbolsL = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    symbolsS = 'abcdefghijklmnopqrstuvwxyz'
-    symbolsN = '1234567890'
-    symbols = '-+=*?!%@_'
-    rand1 = (''.join(choice(symbols) for i in range(cs)))
-    rand2 = (''.join(choice(symbolsL) for i in range(cs)))
-    rand3 = (''.join(choice(symbolsS) for i in range(cs)))
-    rand4 = (''.join(choice(symbolsN) for i in range(cs)))
-    print (rand1) 
-    print (rand2)
-    print (rand3)
-    print (rand4)
+# Константы для символов
+SYMBOLS_L = ascii_uppercase
+SYMBOLS_S = ascii_lowercase
+SYMBOLS_N = digits
+SYMBOLS_SPECIAL = '-+=*?!%@_'
 
-test()
+def generate_random_string(symbols, length):
+    """Генерирует случайную строку заданной длины из предоставленных символов."""
+    return ''.join(choice(symbols) for _ in range(length))
+
+def generate_password(length):
+    """Генерирует пароль, состоящий из разных типов символов."""
+    rand1 = generate_random_string(SYMBOLS_SPECIAL, length)
+    rand2 = generate_random_string(SYMBOLS_L, length)
+    rand3 = generate_random_string(SYMBOLS_S, length)
+    rand4 = generate_random_string(SYMBOLS_N, length)
+    
+    # Объединяем все части в один пароль
+    password = rand1 + rand2 + rand3 + rand4
+    # Перемешиваем символы для большей случайности
+    password = ''.join(random.sample(password, len(password)))
+    
+    return password
+
+def main():
+    cs = random.randint(2, 9)
+    password = generate_password(cs)
+    print("Generated Password:", password)
+
+if __name__ == "__main__":
+    main()
