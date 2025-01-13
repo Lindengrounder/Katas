eda39a1b3d9cd4156b95476f64810500a435a081
Anton Solovev <48914909+Lindengrounder@users.noreply.github.com>
2025-01-14 00:42:16 +0300

    Create KataCalc.js

---
+++ KataCalc.js
@@ -0,0 +1,17 @@
+
+// Get user input for two numbers
+var x = 10000;
+var y = 600;
+
+var z = x + y;
+
+console.log("x + y = " + z);
+var z = x - y;
+
+console.log("x - y = " + z);
+var z = x * y;
+
+console.log("x * y = " + z);
+var z = x / y;
+
+console.log("x / y = " + z);
